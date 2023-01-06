31ccee671f0d296cbfb4a565cac94a293f6bd5e2
Mauricio <mauri.f.mores@gmail.com>
2023-01-06 09:20:51 -0300

    commit end

--- assets/javascript/details.js
+++ assets/javascript/details.js
@@ -11,6 +11,7 @@ let todasLasCards=data.events;
 let cardFiltrada= todasLasCards.find(todasLasCards=>todasLasCards._id==id);
 
 function incertarEventos(card,space){
+  if(card.assistance!=undefined){
     space.innerHTML=`
     <div class="card m-4 bg-dark text-light" >
     <div class="row">
@@ -25,23 +26,58 @@ function incertarEventos(card,space){
           <p class="card-text col-3">Category: ${card.category}</p>
           <p class="card-text col-3">Place: ${card.place}</p>
           <p class="card-text col-3">Capacity: ${card.capacity}</p>
-          <p class="card-text col-3">Asistance: ${card.asistance}</p>
+          <p class="card-text col-3">Asistance: ${card.assistance}</p>
           </div>
           <p class="card-text text-primary">Price: $ ${card.price}</p>
         </div>
       </div>
     </div>
   </div>
-    `;
-    
+    `;}
+    else if(card.estimate!=undefined){
+      space.innerHTML=`
+      <div class="card m-4 bg-dark text-light" >
+      <div class="row">
+          <div class="col-md-4 d-flex justify-content-center">
+              <img src="${card.image}" class="img-fluid rounded-start imagenCards" alt="${card.name}">
+          </div>
+        <div class="col-md-8">
+          <div class="card-body text-center">
+            <h3 class="card-title">${card.name}</h3>
+            <p class="card-text">${card.description}</p>
+            <div class="row gy-5">
+            <p class="card-text col-3">Category: ${card.category}</p>
+            <p class="card-text col-3">Place: ${card.place}</p>
+            <p class="card-text col-3">Capacity: ${card.capacity}</p>
+            <p class="card-text col-3">Estimate: ${card.estimate}</p>
+            </div>
+            <p class="card-text text-primary">Price: $ ${card.price}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+      `;}
+      else{
+        space.innerHTML=`
+        <div class="card m-4 bg-dark text-light" >
+        <div class="row">
+            <div class="col-md-4 d-flex justify-content-center">
+                <img src="${card.image}" class="img-fluid rounded-start imagenCards" alt="${card.name}">
+            </div>
+          <div class="col-md-8">
+            <div class="card-body text-center">
+              <h3 class="card-title">${card.name}</h3>
+              <p class="card-text">${card.description}</p>
+              <div class="row gy-5">
+              <p class="card-text col-4">Category: ${card.category}</p>
+              <p class="card-text col-4">Place: ${card.place}</p>
+              <p class="card-text col-4">Capacity: ${card.capacity}</p>
+              </div>
+              <p class="card-text text-primary">Price: $ ${card.price}</p>
+            </div>
+          </div>
+        </div>
+      </div>
+        `;}
 }
-incertarEventos(cardFiltrada,mainCards)
-/*<div class="card-body">
-<h5 class="card-title">Card title</h5>
-<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-</div>
-<ul class="list-group list-group-flush">
-<li class="list-group-item">An item</li>
-<li class="list-group-item">A second item</li>
-<li class="list-group-item">A third item</li>
-</ul>*/
+incertarEventos(cardFiltrada,mainCards);
