b028b4284dcdc13cd1bd0392a4352bcbbd664860
Mauricio <mauri.f.mores@gmail.com>
2023-01-10 11:06:19 -0300

    change function imprimirseccionuno

--- assets/javascript/stats.js
+++ assets/javascript/stats.js
@@ -8,20 +8,20 @@ fetch("https://mindhub-xj03.onrender.com/api/amazing")//recuperar data por fetch
 .then(data=>data.json())
 .then(datos=>{
     dataDeCars=datos;
-    dataDeCars.events.push({
-        assistance: 42756,
-        capacity: 45000,
-        category: "Food Fair",
-        date: "2021-12-12",
-        description: "Enjoy your favourite dishes, from different countries, in a unique event for the whole family.",
-        image: "https://i.postimg.cc/Fs03hQDt/Collectivities-Party.jpg",
-        name: "Collectivities Party",
-        place: "Room A",
-        price: 5,
-        __v: 0,
-        _id: "5639c723b992482e5f2834be9",
-    });
-    let todasLasEstadisticas=[];
+    // dataDeCars.events.push({
+    //     assistance: 42756,
+    //     capacity: 45000,
+    //     category: "Food Fair",
+    //     date: "2021-12-12",
+    //     description: "Enjoy your favourite dishes, from different countries, in a unique event for the whole family.",
+    //     image: "https://i.postimg.cc/Fs03hQDt/Collectivities-Party.jpg",
+    //     name: "Collectivities Party",
+    //     place: "Room A",
+    //     price: 5,
+    //     __v: 0,
+    //     _id: "5639c723b992482e5f2834be9",
+    // });
+    let todasLasEstadisticas=[];//nuevo array con datos necesarios ya calculados
     for (const card of dataDeCars.events) {
         todasLasEstadisticas.push(
             {
@@ -36,18 +36,15 @@ fetch("https://mindhub-xj03.onrender.com/api/amazing")//recuperar data por fetch
             }
         );
     }
-    let cardsPasadas=todasLasEstadisticas.filter(card=>card.fecha=='past').sort();
-    let cardsPorVenir=todasLasEstadisticas.filter(card=>card.fecha=='upcoming').sort();
-    imprimirSeccionUno(menor_mayorAsistencia(todasLasEstadisticas),eventosEstaticos);
-    imprimirSeccionesDosTres(reductora(cardsPasadas),eventosPasados);
-    imprimirSeccionesDosTres(reductora(cardsPorVenir),eventosPorVenir);
+    let cardsPasadas=todasLasEstadisticas.filter(card=>card.fecha=='past').sort();//filtrar pasadas
+    let cardsPorVenir=todasLasEstadisticas.filter(card=>card.fecha=='upcoming').sort();//filtrar futuras
+    imprimirSeccionUno(todasLasEstadisticas,eventosEstaticos);//llamado de imprecion staticas
+    imprimirSeccionesDosTres(reductora(cardsPasadas),eventosPasados);//llamado de impresion pasadas
+    imprimirSeccionesDosTres(reductora(cardsPorVenir),eventosPorVenir);//llamado de impresion futuras
 })
 .catch(err=>console.log(err));
-function menor_mayorAsistencia(todasLasEstadisticas){
-    todasLasEstadisticas.sort((mayor,menor)=>mayor.porcentaje-menor.porcentaje);
-    return todasLasEstadisticas;
-}
 function imprimirSeccionUno(estadisticas,lugarEnTabla){
+    estadisticas.sort((mayor,menor)=>mayor.porcentaje-menor.porcentaje);
     lugarEnTabla.innerHTML+=`<tr class="table-active color_td" >
                             <td>${estadisticas[estadisticas.length-1].nombreEvento}: ${estadisticas[estadisticas.length-1].porcentaje}%</td>
                             <td>${estadisticas[0].nombreEvento}: ${estadisticas[0].porcentaje}%</td>
